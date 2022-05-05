71944d612b637ed32f6f2ed45ff1e0f6d8636122
Tuan Anh <ncq998@gmail.com>
2022-05-05 22:07:12 +0700

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="">
+            das
+            <span>dsadas</span>
+        </div>
+    )
+}
+
+export default App
