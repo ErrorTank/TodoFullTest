2745811ee40a53a090754ebccc90de3f579966d0
Hoan Nguyen Van <hoan@inspectorio.com>
2022-05-13 11:25:54 +0700

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,11 @@
-import React, { useState } from 'react'
-import {
-  Link,
-  MakeGenerics,
-  Outlet,
-  ReactLocation,
-  Router,
-  useMatch,
-} from '@tanstack/react-location'
+import React from 'react'
+import { ReactLocation, Router } from '@tanstack/react-location'
 import './App.css'
-import HomeRoute from './routes/HomeRoute/HomeRoute'
 
 const location = new ReactLocation()
 
 function App() {
-  const { test, hihi } = location
-
-  const { ab } = abb
-  return (
-    <Router
-      location={location}
-      routes={[
-        { path: '/', element: <HomeRoute /> },
-        {
-          path: 'posts',
-          element: <Posts />,
-          loader: async () => {
-            return {
-              posts: await fetchPosts(),
-            }
-          },
-
-          children: [
-            { path: '/', element: <PostsIndex /> },
-            {
-              path: ':postId',
-              element: <Post />,
-
-              loader: async ({ params: { postId } }) => {
-                return {
-                  post: await fetchPostById(postId),
-                }
-              },
-            },
-          ],
-        },
-      ]}
-    />
-  )
+  return <Router location={location} routes={[]} />
 }
 
 export default App
